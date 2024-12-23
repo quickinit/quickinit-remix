72ab6e9707c11a679ddbb0909bd3fdf0c7a85d8f
Nilesh <nilesh.28.2003@gmail.com>
2024-12-23 10:56:19 +0530

    add: dark mode support , toaster and font

--- app/components/ui/sonner.tsx
+++ app/components/ui/sonner.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { useTheme } from 'next-themes';
+import { useTheme } from 'remix-themes';
 import { Toaster as Sonner } from 'sonner';
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-	const { theme = 'system' } = useTheme();
-
+	const [theme] = useTheme();
 	return (
 		<Sonner
 			theme={theme as ToasterProps['theme']}
